ae6084ecfc4ca600865efb66011018e56b4394ae
Richard Morris <rwmorris.bjj@gmail.com>
2020-10-10 20:38:45 +0100

    updating files

--- todo.rb
+++ todo.rb
@@ -2,6 +2,34 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  erb "You have no lists.", layout: :layout
+  redirect "/lists"
+end
+
+
+# View all the lists
+get "/lists" do
+  @lists = session[:lists]
+  erb :lists, layout: :layout
+end
+
+# Render the new list form
+get "/lists/new" do
+ erb :new_list, layout: :layout
+end
+
+# Create a new list
+post "/lists" do
+  session[:lists] << {name: params[:list_name], todos: []}
+  session[:success] = "The list has been created."
+  redirect "/lists"
 end
