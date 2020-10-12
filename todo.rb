e5b89c32bdc405180665a7c611fdd1d62953f005
Richard Morris <rwmorris.bjj@gmail.com>
2020-10-12 22:02:17 +0100

    updating project files

--- todo.rb
+++ todo.rb
@@ -2,6 +2,7 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
+require "sinatra/content_for"
 require 'tilt/erubis'
 
 configure do
@@ -37,6 +38,14 @@ def error_for_list_name(name)
   end
 end
 
+
+def error_for_todo(name)
+  if !(1..100).cover?(name.size)
+    'The list name must be between 1 and 100 characters.'
+  end
+end
+
+
 # Create a new list
 post '/lists' do
   list_name = params[:list_name].strip
@@ -51,3 +60,60 @@ post '/lists' do
     redirect '/lists'
   end
 end
+
+
+get "/lists/:id" do
+  @list_id = params[:id].to_i
+  @list = session[:lists][@list_id]
+  erb :list, layout: :layout
+end
+
+# Edit an existing todo list
+get "/lists/:id/edit" do
+  id = params[:id].to_i
+  @list = session[:lists][id]
+  erb :edit_list, layout: :layout
+end
+
+# update an existing todo list
+post "/lists/:id" do
+  list_name = params[:list_name].strip
+  id = params[:id].to_i
+  @list = session[:lists][id]
+
+  error = error_for_list_name(list_name)
+  if error
+    session[:error] = error
+    erb :edit_list, layout: :layout
+  else
+    @list[:name] = list_name
+    session[:success] = 'The list has been updated.'
+    redirect "/lists/#{id}"
+  end
+end
+
+# Delete a todo list
+post "/lists/:id/destroy" do
+  id = params[:id].to_i
+  session[:lists].delete_at(id)
+  session[:success] = 'The list has been deleted.'
+  redirect "/lists"
+end
+
+# Add a new todo to a list
+post "/lists/:list_id/todos" do
+  @list_id = params[:list_id].to_i
+  @list = session[:lists][@list_id]
+  text = params[:todo].strip
+
+  error = error_for_todo(text)
+  if error
+    session[:error] = error
+    erb :list, layout: :layout
+  else
+    @list[:todos] << {name: params[:todo], completed: false}
+    session[:success] = 'The todo has been added.'
+    redirect "/lists/#{@list_id}"
+  end
+end
+
